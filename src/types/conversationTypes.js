54b4a3564559d9427328e5ff4a5f372d67ef4476
Alondra Castro <44213141+Nonita16@users.noreply.github.com>
2021-03-30 20:46:50 -0400

    Not finished but working fine

---
+++ src/types/conversationTypes.js
@@ -0,0 +1,6 @@
+
+export const GET_CONVERSATION = 'get_conversations'
+export const SORT_CHATLIST = 'sort_conversations'
+export const GET_CHATLIST = 'get_chatlist'
+export const LOADING = 'LOADING'
+export const ERROR = 'ERROR'
